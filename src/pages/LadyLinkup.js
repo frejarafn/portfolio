4c80b3de75aa4a3b4e41252edf75365f14acc8e4
Freja van Gilst <frejavangilst@gmail.com>
2024-04-09 10:57:17 +0200

    Update image width and add project description in LadyLinkup.js

--- src/pages/LadyLinkup.js
+++ src/pages/LadyLinkup.js
@@ -20,11 +20,32 @@ export default function LadyLinkup() {
                         The theme of the project was open to interpretation, so I decided to build a platform where women could create and join different types of events and connect with each other.
                     </p>
                 </div>
-                <div>
-                    <img src="/images/ladylinkup-1.png" alt="Lady LinkUp" />
-                    <img src="/images/ladylinkup-2.png" alt="Lady LinkUp" />
+                <img src="/images/ladylinkup-1.png" alt="Lady LinkUp" className="project-image" />
+
+                <div className="project-about">
+                    <div>
+                        <h3>THE PROBLEM</h3>
+                        <p>
+                            Since the project was open to interpretation, I had to come up with a problem that I wanted to solve. I wanted to create a platform were women could connect with other women through interests and events.
+                            I wanted to create a safe space with events that people actually wanted to attend and where they could meet other people and make new friends. The platform should be easy to use and navigate and have a clean and simple design.
+                        </p>
+                    </div>
                 </div>
 
+            </div>
+            <div className="project-about">
+                <div>
+                    <h3>THE SOLUTION</h3>
+                    <p>
+                        In my solution I focused on making the platform user-friendly and easy to navigate. I made it possible for the user to sign up and log in to the site, create a profile, edit a profile, create events, join events and delete events.
+                        You have to be logged in as a user to create an event but you do not have to be logged in to see the events that are available. I achieved this by using Remix, Remix auth, Bcrypt, MongoDB, Mongoose, Tailwind CSS and Render.
+                        I made sure to hash the users passwords and store them in the database and I made sure to validate the users input to prevent any malicious code from being injected into the database.
+                        I ended up with a simple but fully functional platform were the user is able to do anyhthing I set out to do in the beginning.
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="/images/ladylinkup-2.png" alt="Lady LinkUp" className="project-image" />
             </div>
         </div>
     );
